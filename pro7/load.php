b5a73bb87aa57d6e586f6d518ad728e7c3e8e9d2
ccblazer44 <cblaze@BallerBook.local>
2016-02-21 19:41:18 -0700

    removed pro7

--- pro7/load.php
+++
@@ -1,6 +0,0 @@
-<?php
-	//loading required files
-	require_once(dirname(__FILE__) . '/config.php');
-	require_once(dirname(__FILE__) . '/databaseandclass/db.php');
-	require_once(dirname(__FILE__) . '/databaseandclass/class.php');
-?>
