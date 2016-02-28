3ca7e88e7604bf24b74af1a40c594b539c74b251
Jorden <jakereps@users.noreply.github.com>
2016-02-28 16:50:48 -0700

    Pseudo-stylize registration page

--- users/config.php
+++ users/config.php
@@ -1,11 +1,19 @@
 <?php
-//Starts the session
-session_start();
 
-//Connecticn my database
-mysql_connect('tund.cefns.nau.edu', 'cb367', 'saga44');
-mysql_select_db('cb367');
 
+$mysql_server = "tund.cefns.nau.edu";
+$mysql_user = "cb367";
+$mysql_password = "saga44";
+$mysql_db = "cb367";
+$mysqli = new mysqli($mysql_server, $mysql_user, $mysql_password, $mysql_db);
+if ($mysqli->connect_errno) {
+  printf("Connection failed: %s \n", $mysqli->connect_error);
+  exit();
+}
+
+$mysqli->set_charset("utf8");
+
+  session_start();
 //The Home MSGI Page
 $url_home = 'index.php';
 
