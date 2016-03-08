ededa7c9a73e729a9a6e74fc9e7f10f917cde366
Jorden <jakereps@users.noreply.github.com>
2016-03-08 15:17:36 -0700

    Change menu, Rename image folder

--- js/menu.js
+++ js/menu.js
@@ -2,6 +2,12 @@ jQuery(document).ready(function() {
 	jQuery('.toggle-nav').click(function(e) {
 			jQuery(this).toggleClass('active');
 			jQuery('#main-nav ul').toggleClass('active');
+
+			if (jQuery(this).hasClass('active')){
+				jQuery(this).html("&#10006;");
+			} else {
+				jQuery(this).html("&#9776;");
+			}
 			e.preventDefault();
 	});
 });
